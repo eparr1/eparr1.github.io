3bcbc7022bae1384b748043e2426f3a1f615f050
eparr1 <emmaparr12345@gmail.com>
2024-09-06 12:45:07 +0100

    contact save

--- src/app/Components/UI/contact.tsx
+++ src/app/Components/UI/contact.tsx
@@ -1,21 +1,25 @@
 'use client'
 import React from "react";
 import Image from "next/image";
+
 const Contact = () => {
+    // Function to handle opening mailto link in a new tab
     const openEmailClient = () => {
         window.open('mailto:emmaparr12345@gmail.com', '_blank');
     };
 
+    // Function to handle opening phone dialer
     const openPhoneDialer = () => {
         window.open('tel:07463268450', '_blank');
     };
 
+    // Function to handle opening Google Maps
     const openGoogleMaps = () => {
-        window.open('https://www.google.com/maps/dir//Edinburgh,+Scotland/@55.9532245,-3.2706668,12z/data=!3m1!4b1!4m13!4m12!1m0!1m5!1m1!1s0x4887b800a5982623:0x64f2147b7ce71727!2m2!1d-3.188267!2d55.953252!2m3!6e0!7e2!8j1723444200!3e3?entry=ttu&g_ep=EgoyMDI0MDkwMy4wIKXMDSoASAFQAw%3D%3D', '_blank')
-    }
+        window.open('https://www.google.com/maps?q=Edinburgh,+Scotland', '_blank');
+    };
 
     return (
-        <div className=" flex flex-col items-center justify-center gap-[50px] m-auto text-white">
+        <div className="flex flex-col items-center justify-center gap-[50px] m-auto text-white">
             <div className="relative text-4xl font-semibold">
                 <h1>Get in Touch</h1>
             </div>
@@ -34,45 +38,47 @@ const Contact = () => {
 
                     {/* Icons Section */}
                     <div className="flex flex-col gap-[20px]">
+                        {/* Email Link */}
                         <div className="flex items-center gap-5">
-                        <a  onClick={openEmailClient} className="flex items-center gap-5 cursor-pointer">
-                            <Image
-                                src='/mail.svg'
-                                alt='email address'
-                                width={50}
-                                height={50}
-                                className="bg-gradient-to-b from-purple-400 to-pink-700 p-3 rounded-xl"
-                            />
-                            <p>emmaparr12345@gmail.com</p>
-                        </a>
+                            <a onClick={openEmailClient} className="flex items-center gap-5 cursor-pointer">
+                                <Image
+                                    src='/mail.svg'
+                                    alt='email address'
+                                    width={50}
+                                    height={50}
+                                    className="bg-gradient-to-b from-purple-400 to-pink-700 p-3 rounded-xl"
+                                />
+                                <p>emmaparr12345@gmail.com</p>
+                            </a>
                         </div>
-                        
+
+                        {/* Phone Link */}
                         <div className="flex items-center gap-5">
-                        <a onClick={openPhoneDialer} className="flex items-center gap-5 cursor-pointer">
-                            <Image
-                                src='/phoneIcon.svg'
-                                alt='phone number'
-                                width={50}
-                                height={50}
-                                className="bg-gradient-to-b from-purple-400 to-pink-700 p-3 rounded-xl"
-                            />
-                            <p>07463268450</p>
-                        </a>
+                            <a onClick={openPhoneDialer} className="flex items-center gap-5 cursor-pointer">
+                                <Image
+                                    src='/phoneIcon.svg'
+                                    alt='phone number'
+                                    width={50}
+                                    height={50}
+                                    className="bg-gradient-to-b from-purple-400 to-pink-700 p-3 rounded-xl"
+                                />
+                                <p>07463268450</p>
+                            </a>
                         </div>
-                        
+
+                        {/* Location Link to Google Maps */}
                         <div className="flex items-center gap-5">
-                        <a onClick={openGoogleMaps} className='flex items-center gap-5 cursor-pointer'>
-                            <Image
-                                src='/locationIcon.svg'
-                                alt='location'
-                                width={50}
-                                height={50}
-                                className="bg-gradient-to-b from-purple-400 to-pink-700 p-3 rounded-xl"
-                            />
-                            <p>Edinburgh, Scotland</p>
-                        </a>
+                            <a onClick={openGoogleMaps} className="flex items-center gap-5 cursor-pointer">
+                                <Image
+                                    src='/locationIcon.svg'
+                                    alt='location'
+                                    width={50}
+                                    height={50}
+                                    className="bg-gradient-to-b from-purple-400 to-pink-700 p-3 rounded-xl"
+                                />
+                                <p>Edinburgh, Scotland</p>
+                            </a>
                         </div>
-                       
                     </div>
                 </div>
 
@@ -80,15 +86,36 @@ const Contact = () => {
                 <div className="flex flex-col items-start gap-3 text-[#D8D8D8]">
                     <form className="flex flex-col gap-4">
                         <label htmlFor="name">Your Name</label>
-                        <input type='text' id='name' placeholder="Enter your name" name='name' className="p-4 bg-[#32323c] rounded" />
+                        <input
+                            type='text'
+                            id='name'
+                            placeholder="Enter your name"
+                            name='name'
+                            className="p-4 bg-[#32323c] rounded"
+                        />
                         
                         <label htmlFor='email'>Your Email</label>
-                        <input type='email' id='email' placeholder="Enter your email" name='email' className="p-4 bg-[#32323c] rounded" />
+                        <input
+                            type='email'
+                            id='email'
+                            placeholder="Enter your email"
+                            name='email'
+                            className="p-4 bg-[#32323c] rounded"
+                        />
                         
                         <label htmlFor="message">Write your message here</label>
-                        <textarea name="message" id="message" rows={8} placeholder="Enter your message" className="p-4 bg-[#32323c] text-p-4 text-[#A0A0A0] rounded w-[650px]" />
+                        <textarea
+                            name="message"
+                            id="message"
+                            rows={8}
+                            placeholder="Enter your message"
+                            className="p-4 bg-[#32323c] text-[#A0A0A0] rounded w-[650px]"
+                        />
                         
-                        <button type="submit" className="text-white cursor-pointer rounded-lg transition-[0.3s] w-1/3  mt-4 bg-gradient-to-b from-purple-400 to-pink-700 px-4 py-2 mb-4 bg-indigo-600rounded">
+                        <button
+                            type="submit"
+                            className="text-white cursor-pointer rounded-lg transition duration-300 w-1/3 mt-4  bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 px-4 py-2"
+                        >
                             Send Message
                         </button>
                     </form>
@@ -98,5 +125,5 @@ const Contact = () => {
     );
 };
 
+export default Contact;
 
-export default Contact
